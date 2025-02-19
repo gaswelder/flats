14de80678c5d432c29241b82e8c3367da00c1359
gasweld <gasweld@gmail.com>
2025-02-19 13:05:01 +0200

    Cleanup history charts

--- frontend/components/Stats.js
+++ frontend/components/Stats.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { VictoryChart, VictoryHistogram, VictoryTheme } from "victory";
-import { AveragesPlot } from "./AveragesPlot";
-import { CountsPlot } from "./CountsPlot";
+import {
+  VictoryAxis,
+  VictoryChart,
+  VictoryHistogram,
+  VictoryLine,
+  VictoryTheme,
+} from "victory";
+import { fetchAverages, fetchOfferCounts } from "../state";
 
 const Div = styled.div`
   & figure {
@@ -15,6 +20,41 @@ const Div = styled.div`
 `;
 
 export const Stats = ({ offers, area, filter }) => {
+  const [prices, setPrices] = useState(null);
+  const [pricesErr, setPricesErr] = useState(null);
+  const [counts, setCounts] = useState();
+  const [countsErr, setCountsErr] = useState(null);
+
+  useEffect(() => {
+    const latBounds = [area.leftTop.latitude, area.rightBottom.latitude];
+    const lonBounds = [area.leftTop.longitude, area.rightBottom.longitude];
+    fetchAverages(filter.rooms, latBounds, lonBounds).then((result) => {
+      if (Array.isArray(result)) {
+        setPrices(result);
+      } else {
+        setPricesErr("failed to load data");
+      }
+    });
+    fetchOfferCounts(filter.rooms, latBounds, lonBounds, filter.price[1]).then(
+      (result) => {
+        if (Array.isArray(result)) {
+          setCounts(result);
+        } else {
+          setCountsErr("failed to load data");
+        }
+      }
+    );
+  }, [area, filter]);
+
+  if (countsErr) return countsErr;
+  if (pricesErr) return pricesErr;
+  if (!counts) {
+    return "Loading...";
+  }
+
+  const dataPrices = prices.map((p) => ({ x: new Date(p.ts), y: p.price }));
+  const dataCounts = counts.map((p) => ({ x: new Date(p.ts), y: p.count }));
+
   return (
     <Div>
       <figure>
@@ -23,11 +63,36 @@ export const Stats = ({ offers, area, filter }) => {
           <VictoryHistogram data={offers.map((x) => ({ x: x.price }))} />
         </VictoryChart>
       </figure>
-      <AveragesPlot area={area} filter={filter} />
-      <figure>
-        <figcaption>Offer count over time</figcaption>
-        <CountsPlot area={area} filter={filter} />
-      </figure>
+
+      <div style={{ width: "500px" }}>
+        <XYPlot xys={dataPrices} noxaxis title="Prices" />
+        <div style={{ position: "relative", top: -100 }}>
+          <XYPlot xys={dataCounts} title="Counts" />
+        </div>
+      </div>
     </Div>
   );
+};
+
+const XYPlot = ({ xys, noxaxis, title }) => {
+  return (
+    <div style={{ position: "relative" }}>
+      <p style={{ position: "absolute", left: 80, top: 80 }}>{title}</p>
+      <VictoryChart height={200}>
+        <VictoryAxis
+          crossAxis
+          tickFormat={noxaxis ? () => null : formatDateTick}
+        />
+        <VictoryAxis dependentAxis />
+        <VictoryLine data={xys} />
+      </VictoryChart>
+    </div>
+  );
+};
+
+const formatDateTick = (x) => {
+  const date = new Date(x);
+  const m = date.getMonth() + 1;
+  const d = date.getDate();
+  return d.toString().padStart(2, 0) + "." + m.toString().padStart(2, 0);
 };
