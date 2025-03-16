f29141049fc76efef8be6b3dbbad6f7b623a4241
gasweld <gasweld@gmail.com>
2025-03-16 22:15:06 +0200

    Use ms for durations

--- backend/timer.mjs
+++ backend/timer.mjs
@@ -2,8 +2,8 @@ export const timer = (log) => {
   let t = Date.now();
   return (name, n) => {
     const t2 = Date.now();
-    const dt = t2 - t;
+    const ms = t2 - t;
     t = t2;
-    log.info("timer", { name, dt, n });
+    log.info("timer", { name, ms, n });
   };
 };
